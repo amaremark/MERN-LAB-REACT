da4ba997de8369b832b7a5f843101b43a93ed2eb
Amare AbebeE <amaremark@gmail.com>
2019-02-12 13:21:50 -0500

    front end

---
+++ bucketlist/src/App.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Navbar } from "react-bootstrap";
+import { NavItem } from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
+import CreateItem from "./CreateItem/createItem";
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,Switch ,
+  // Redirect
+      } from 'react-router-dom'
+import ListItem from './ListItem/ListItem';
+
+
+ class App extends Component {
+  state = {
+    title: this.title,
+    url: this.url,
+    discription: this.discription
+  }
+
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <header className="App-header">
+
+            <Navbar inverse collapseOnSelect>
+              <Navbar.Header>
+                <Navbar.Brand>
+                  <a href="/">React-BucketList</a>
+                </Navbar.Brand>
+                <Navbar.Toggle />
+              </Navbar.Header>
+              <Navbar.Collapse>
+                <Nav pullRight>
+                  <NavItem eventKey={1} href="#">
+                    <Link to="/createitem" style={{ textDecoration: 'none', color: "lightblue" }}>
+                      Create Item
+                    </Link>
+                  </NavItem>
+                  <NavItem>
+                    <Link to="/listItems">List</Link>
+                  </NavItem>
+                </Nav>
+              </Navbar.Collapse>
+            </Navbar>
+          </header>
+          
+            <h1>Bucket List!</h1>
+            <p>
+              This is a simple hero application where you can keep track of your bucket list progress </p>
+            <p>
+            </p>
+        
+          <section className="form">
+          </section>
+          <main>
+            <Switch>
+              <Route path="/createitem" render={props => <CreateItem {...this.state} {...props} stocks={this.props.stocks} />}/>
+               
+              <Route path="/listItems" Component={ListItem} />
+            </Switch>
+          </main>
+        </div>
+      </Router >
+    )
+    }
+  }
+
+  export default App;
